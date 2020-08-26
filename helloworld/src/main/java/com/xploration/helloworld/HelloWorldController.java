3fdffdc8359eded02b414df8d96b6bd7c2da73c1
venkateshkumard <venkateshkumar_d@yahoo.com>
2020-08-26 17:34:45 +0530

    Add files via upload
    testing

---
+++ helloworld/src/main/java/com/xploration/helloworld/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.xploration.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/helloworld")
+public class HelloWorldController{
+
+
+    @GetMapping(path="/{name}")
+    public String getMessage(@PathVariable("name") String name){
+        return "Hello World... Welcome "+name;
+    }
+
+}
